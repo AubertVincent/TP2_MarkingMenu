c1b79b8a7f9ab99b053c7031bb86954a1627b28a
AubertVincent <va.aubert1421@gmail.com>
2018-10-25 22:46:46 +0200

    Color master menu not visible

--- src/vue/Vue.java
+++ src/vue/Vue.java
@@ -24,14 +24,12 @@ import java.util.Vector;
 import javax.swing.AbstractAction;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
 import javax.swing.event.MouseInputAdapter;
 import javax.swing.event.MouseInputListener;
 
 import controleur.Controleur;
-
 
 public class Vue extends JFrame {
 	Vector<Shape> shapes = new Vector<Shape>();
@@ -40,7 +38,7 @@ public class Vue extends JFrame {
 	public Color currentColor;
 	Controleur control;
 	JPanel menu;
-	
+
 	class Tool extends AbstractAction implements MouseInputListener {
 		Point o;
 		Shape shape;
@@ -48,9 +46,9 @@ public class Vue extends JFrame {
 
 		public Tool(String name) {
 			super(name);
-			this.name=name;
+			this.name = name;
 		}
-		
+
 		public String getName() {
 			return name;
 		}
@@ -87,6 +85,8 @@ public class Vue extends JFrame {
 		public void mouseMoved(MouseEvent e) {
 		}
 	}
+
+	Color colors[] = { Color.BLACK, Color.RED, Color.BLUE, Color.YELLOW, Color.GREEN };
 
 	Tool tools[] = { new Tool("pen") {
 		public void mouseDragged(MouseEvent e) {
@@ -138,7 +138,7 @@ public class Vue extends JFrame {
 			{
 				for (AbstractAction tool : tools) {
 					add(tool);
-				} 
+				}
 				String[] items = { "black", "red", "green", "yellow", "blue" };
 				JComboBox<String> color = new JComboBox<String>(items);
 				color.addItemListener(new ItemListener() {
@@ -188,82 +188,108 @@ public class Vue extends JFrame {
 				}
 			}
 		};
-		
-			
-//		menu.setVisible(false);
+
+		// menu.setVisible(false);
 
 		MouseInputAdapter listener = new MouseInputAdapter() {
-			
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				Point p = e.getPoint();
-//				if (menu.isVisible()) {
-//
-//					menu.setVisible(false);
-//				} else {
-//					menu.setLocation(p.x-menu.getWidth()/2,p.y-menu.getHeight()/2);
-//					menu.setVisible(true);
-//					menu.repaint();
-//				}
-				if(menu==null) {
+				// if (menu.isVisible()) {
+				//
+				// menu.setVisible(false);
+				// } else {
+				// menu.setLocation(p.x-menu.getWidth()/2,p.y-menu.getHeight()/2);
+				// menu.setVisible(true);
+				// menu.repaint();
+				// }
+				if (menu == null) {
 					menu = createMenu();
 					panel.add(menu);
-					menu.setBounds(p.x-menu.getWidth()/2,p.y-menu.getHeight()/2, 200, 200);
-				}else {
+					menu.setBounds(p.x - menu.getWidth() / 2, p.y - menu.getHeight() / 2, 200, 200);
+				} else {
 					panel.remove(menu);
 					panel.repaint();
-					menu=null;
+					menu = null;
 				}
-				
+
 			}
-			
-			
+
 		};
-		
-		
 
 		panel.addMouseListener(listener);
 		add(panel);
-		
 
 		pack();
 		setVisible(true);
 	}
-	
+
 	public JPanel createMenu() {
 		JPanel menu = new JPanel() {
 			{
 				setOpaque(false);
 			}
 		};
-		menu.setBounds(0,0,200,200);
+		menu.setBounds(0, 0, 200, 200);
 
 		panel.setLayout(null);
 
-		MarkingMenu markmenu = new MarkingMenu(menu.getWidth(),menu.getHeight());
-		
+		MarkingMenu markmenu = new MarkingMenu(menu.getWidth(), menu.getHeight());
+
 		menu.setUI(markmenu);
-		
-		
-		
-		//MOUSE LISTENER OUTILS
-		for(int i=0;i<tools.length;i++) {
-			final int j =i; 
-			markmenu.addToolLabel(tools[j].getName());
-			
+
+		for (int i = 0; i < colors.length; i++) {
+			final int j = i;
+			markmenu.addColor(colors[j]);
+
 			MouseInputAdapter menulistener = new MouseInputAdapter() {
 				@Override
 				public void mouseMoved(MouseEvent e) {
-					if(markmenu.getCurrentState()==StateMenu.TOOLS && markmenu.getArcsTools().get(j).contains(e.getPoint())) {
+					if (markmenu.getCurrentState() == StateMenu.COLORS
+							&& markmenu.getArcsColors().get(j).contains(e.getPoint())) {
 						markmenu.setActivate(j);
 						menu.repaint();
 					}
 				}
-				
+
 				@Override
 				public void mouseClicked(MouseEvent e) {
-					if(markmenu.getCurrentState()==StateMenu.TOOLS) {
-						if(markmenu.getArcsTools().get(j).contains(e.getPoint())) {
+					if (markmenu.getCurrentState() == StateMenu.COLORS) {
+						if (markmenu.getArcsColors().get(j).contains(e.getPoint())) {
+							System.out.println("using color " + colors[j]);
+							markmenu.setCurrentState(StateMenu.CHOICE);
+							menu.setVisible(false);
+						} else {
+							menu.setVisible(false);
+						}
+					}
+
+				}
+			};
+			menu.addMouseMotionListener(menulistener);
+			menu.addMouseListener(menulistener);
+		}
+
+		// MOUSE LISTENER OUTILS
+		for (int i = 0; i < tools.length; i++) {
+			final int j = i;
+			markmenu.addToolLabel(tools[j].getName());
+
+			MouseInputAdapter menulistener = new MouseInputAdapter() {
+				@Override
+				public void mouseMoved(MouseEvent e) {
+					if (markmenu.getCurrentState() == StateMenu.TOOLS
+							&& markmenu.getArcsTools().get(j).contains(e.getPoint())) {
+						markmenu.setActivate(j);
+						menu.repaint();
+					}
+				}
+
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					if (markmenu.getCurrentState() == StateMenu.TOOLS) {
+						if (markmenu.getArcsTools().get(j).contains(e.getPoint())) {
 							System.out.println("using tool " + tools[j]);
 							panel.removeMouseListener(tool);
 							panel.removeMouseMotionListener(tool);
@@ -272,97 +298,89 @@ public class Vue extends JFrame {
 							panel.addMouseMotionListener(tool);
 							markmenu.setCurrentState(StateMenu.CHOICE);
 							menu.setVisible(false);
-						}else {
+						} else {
 							menu.setVisible(false);
 						}
 					}
-					
-					
+
 				}
 			};
 			menu.addMouseMotionListener(menulistener);
 			menu.addMouseListener(menulistener);
 		}
-		
+
 		markmenu.addChoiceLabel("Outils");
-		
+
 		MouseInputAdapter menulistener = new MouseInputAdapter() {
 			@Override
 			public void mouseMoved(MouseEvent e) {
-				if(markmenu.getCurrentState()==StateMenu.CHOICE && markmenu.getArcsChoices().get(0).contains(e.getPoint())) {
+				if (markmenu.getCurrentState() == StateMenu.CHOICE
+						&& markmenu.getArcsChoices().get(0).contains(e.getPoint())) {
 					markmenu.setActivate(0);
 					menu.repaint();
 				}
 			}
-			
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				if(markmenu.getCurrentState()==StateMenu.CHOICE) {
-					if(markmenu.getArcsChoices().get(0).contains(e.getPoint())) {
+				if (markmenu.getCurrentState() == StateMenu.CHOICE) {
+					if (markmenu.getArcsChoices().get(0).contains(e.getPoint())) {
 						markmenu.setCurrentState(StateMenu.TOOLS);
 						menu.repaint();
-//						System.out.println("using tool " + tools[j]);
-//						panel.removeMouseListener(tool);
-//						panel.removeMouseMotionListener(tool);
-//						tool = tools[j];
-//						panel.addMouseListener(tool);
-//						panel.addMouseMotionListener(tool);
-					}else {
+					} else {
 						menu.setVisible(false);
 					}
 				}
-				
-				
+
 			}
 		};
-		
+
 		menu.addMouseMotionListener(menulistener);
 		menu.addMouseListener(menulistener);
-		
-		
+
 		markmenu.addChoiceLabel("Couleurs");
-		
+
 		menulistener = new MouseInputAdapter() {
 			@Override
 			public void mouseMoved(MouseEvent e) {
-				if(markmenu.getCurrentState()==StateMenu.CHOICE && markmenu.getArcsChoices().get(1).contains(e.getPoint())) {
+				if (markmenu.getCurrentState() == StateMenu.CHOICE
+						&& markmenu.getArcsChoices().get(1).contains(e.getPoint())) {
 					markmenu.setActivate(1);
 					menu.repaint();
 				}
 			}
-			
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
-//				if(markmenu.getArcsTools().get(j).contains(e.getPoint())) {
-//					System.out.println("using tool " + tools[j]);
-//					panel.removeMouseListener(tool);
-//					panel.removeMouseMotionListener(tool);
-//					tool = tools[j];
-//					panel.addMouseListener(tool);
-//					panel.addMouseMotionListener(tool);
-//				}else {
-//					menu.setVisible(false);
-//				}
-				
+				if (markmenu.getCurrentState() == StateMenu.CHOICE) {
+					if (markmenu.getArcsChoices().get(1).contains(e.getPoint())) {
+						markmenu.setCurrentState(StateMenu.COLORS);
+						menu.repaint();
+					} else {
+						menu.setVisible(false);
+					}
+				}
+
 			}
 		};
-		
+
 		menu.addMouseMotionListener(menulistener);
 		menu.addMouseListener(menulistener);
-		
+
 		return menu;
+
 	}
-	
+
 	public void updateShapes(Vector<Shape> shapes) {
 		this.shapes = shapes;
 	}
-	
+
 	public void updateColors(HashMap<Shape, Color> color) {
 		this.color = color;
 	}
-	
+
 	public void updateCurrentColor(Color c) {
 		this.currentColor = c;
 	}
-	
+
 }
